7173bde4c3d61e6045b39ba72e0cbbcacec1039b
Luis Thieme <guestuser@MacBook-Pro-von-GuestUser.local>
2023-09-09 01:08:45 +0200

    some styling for the login page

--- first-react-app/src/components/LoginButton.tsx
+++ first-react-app/src/components/LoginButton.tsx
@@ -2,7 +2,7 @@ import '../../css/styles.css'
 
 export default function LoginButton() {
     function handleButtonClick() {
-        window.location.href = "html/login.html"
+        window.location.href = "../html/login.html"
     }
 
     return <button className='loginButton' onClick={handleButtonClick}>Login</button>
--- first-react-app/src/components/LoginCard.tsx
+++ first-react-app/src/components/LoginCard.tsx
@@ -6,12 +6,25 @@ export default function LoginCard() {
     return (
         <>
             <div className='login-card'>
-                <h2 className='login-card-label'>Login</h2>
-                <h3 className='centered'>Username</h3>
-                <input type="text" className='centered' />
-                <h3 className='centered'>Password</h3>
-                <input type="text" className='centered' />
-                <button className='centered'>Login</button>
+                <div className="login-card-inner-container">
+                    <h2 className='login-card-label'>Login</h2>
+                </div>
+                <div className="login-card-inner-container">
+                    <h3 className='centered'>Username</h3>
+                </div>
+                <div className="login-card-inner-container">
+                    <input type="text" className='centered login-card-input' />
+                </div>
+                <div className="login-card-inner-container">
+                    <h3 className='centered'>Password</h3>
+                </div>
+                <div className="login-card-inner-container">
+                    <input type="text" className='centered login-card-input' />
+                </div>
+                <div className="login-card-inner-container">
+                    <button className='centered login-card-button'>Login</button>
+                </div>
+
             </div>
         </>
     )
